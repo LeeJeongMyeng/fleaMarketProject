6325c37842461e147dc7897ac225114f92d8449b
이정명(Lee Jeong Myeong) <112403307+LeeJeongMyeng@users.noreply.github.com>
2023-02-24 17:17:47 +0900

    d

--- fleaMarket/src/main/webapp/erd/Member.sql
+++ fleaMarket/src/main/webapp/erd/Member.sql
@@ -1,5 +1,17 @@
 
 CREATE TABLE fleamarketmember(
-id varchar2(100),
-pass varchar2(100)
+(
+	email varchar2(50) NOT NULL,
+	password varchar2(50),
+	nickname varchar2(100),
+	personalNumber varchar2(100),
+	phoneNumber varchar2(100),
+	address varchar2(300),
+	authority char,
+	businessNumber varchar2(20),
+	kakaoemail varchar2(100),
+	naveremail varchar2(100),
+	-- 셀러들이 주로 판매하는 상품주제
+	category varchar2(100),
+	PRIMARY KEY (email)
 );
