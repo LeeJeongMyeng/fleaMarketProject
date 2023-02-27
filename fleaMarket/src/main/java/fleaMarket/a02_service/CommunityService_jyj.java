7568a5a8aac666b7eb9c3ab42f92c5f00e71220e
youjean1101 <114139279+youjean1101@users.noreply.github.com>
2023-02-27 21:06:42 +0900

    0227 mybatis, config 수정

--- fleaMarket/src/main/java/fleaMarket/a02_service/CommunityService_jyj.java
+++ fleaMarket/src/main/java/fleaMarket/a02_service/CommunityService_jyj.java
@@ -1,5 +1,19 @@
 package fleaMarket.a02_service;
 
-public class CommunityService_jyj {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import fleaMarket.a03_dao.CommunityDao_jyj;
+import vo.BoardImg;
+import vo.Capplication;
+
+public class CommunityService_jyj {
+	@Autowired(required=false)
+	private CommunityDao_jyj dao;
+	
+	public void communityInsert(Capplication ins) {
+		dao.communityInsert(ins);
+	}
+	public void communityFileInsert(BoardImg fins) {
+		dao.communityFileInsert(fins);
+	}
 }
--- fleaMarket/src/main/java/fleaMarket/a03_dao/CommunityDao_jyj.java
+++ fleaMarket/src/main/java/fleaMarket/a03_dao/CommunityDao_jyj.java
@@ -1,9 +1,10 @@
 package fleaMarket.a03_dao;
 
+import vo.BoardImg;
+import vo.Capplication;
 
 //fleaMarket.a03_dao.CommunityDao_jyj
 public interface CommunityDao_jyj {
-	
-	//public void insertCommunity(Capplicaion f);
-	//public void insertCommunityFile(BoardImg f);
+	public void communityInsert(Capplication ins);
+	public void communityFileInsert(BoardImg fIns);
 }
