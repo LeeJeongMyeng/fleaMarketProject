44977266cbc2365659b56929a5d5715546d4377b
miok0428 <gimd23214@gmail.com>
2023-03-01 00:58:22 +0900

    vhh

--- fleaMarket/src/main/java/fleaMarket/a01_controller/Req3000_Controller.java
+++ fleaMarket/src/main/java/fleaMarket/a01_controller/Req3000_Controller.java
@@ -1,21 +1,38 @@
 package fleaMarket.a01_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import fleaMarket.a02_service.Req3000_Service;
+import vo.FleaMarket;
+
+
 
 
 
 @Controller("fleamarket")
 public class Req3000_Controller {
-
+	private Req3000_Service service;
+	@Autowired
 //http://localhost:7030/fleaMarket/fRegistration.do
 	@RequestMapping("fRegistration.do")
 	public String fRegistration() {
 	
 		return "FleaMarketRegistration";
 	}
+
+	 @RequestMapping("/fleaMarketins.do") 
+	 public String fleaMarketins(FleaMarket ins, Model d) { 
+		System.out.println(ins.getTitle());
+		 
+			/* service.insertFleaMarket(ins); */
+		 return "redirect:fRegistration.do"; 
+	}
+
 	
 	@RequestMapping("fileupload.do")
 	public String fileupload(@RequestParam("filePath") MultipartFile profile) {
