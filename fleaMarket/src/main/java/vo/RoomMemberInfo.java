6d26c42c214c5e2338ee124e45683c41ce87005b
youjean1101 <114139279+youjean1101@users.noreply.github.com>
2023-03-04 20:00:57 +0900

    정유진 똥닦는중

---
+++ fleaMarket/src/main/java/vo/RoomMemberInfo.java
@@ -0,0 +1,158 @@
+package vo;
+// vo.RoomMemberInfo
+public class RoomMemberInfo {
+	private String communitynumber;
+	private String title;
+	private String content;
+	private String registdate;
+	private String updatedate;
+	private String category;
+	private String email;
+	private String hashtag;
+	private int viewcnt;
+	private int likecnt;
+	private String imgpath;
+	private String password;
+	private String nickname;
+	private String personalnumber;
+	private String phonenumber;
+	private String address;
+	private String authority;
+	private String busnessnumber;
+	private String kakaoemail;
+	private String naveremail;
+	private String name;
+	private String profileimg;
+	public String getCommunitynumber() {
+		return communitynumber;
+	}
+	public void setCommunitynumber(String communitynumber) {
+		this.communitynumber = communitynumber;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegistdate() {
+		return registdate;
+	}
+	public void setRegistdate(String registdate) {
+		this.registdate = registdate;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHashtag() {
+		return hashtag;
+	}
+	public void setHashtag(String hashtag) {
+		this.hashtag = hashtag;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+	public int getLikecnt() {
+		return likecnt;
+	}
+	public void setLikecnt(int likecnt) {
+		this.likecnt = likecnt;
+	}
+	public String getImgpath() {
+		return imgpath;
+	}
+	public void setImgpath(String imgpath) {
+		this.imgpath = imgpath;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPersonalnumber() {
+		return personalnumber;
+	}
+	public void setPersonalnumber(String personalnumber) {
+		this.personalnumber = personalnumber;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	public String getBusnessnumber() {
+		return busnessnumber;
+	}
+	public void setBusnessnumber(String busnessnumber) {
+		this.busnessnumber = busnessnumber;
+	}
+	public String getKakaoemail() {
+		return kakaoemail;
+	}
+	public void setKakaoemail(String kakaoemail) {
+		this.kakaoemail = kakaoemail;
+	}
+	public String getNaveremail() {
+		return naveremail;
+	}
+	public void setNaveremail(String naveremail) {
+		this.naveremail = naveremail;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProfileimg() {
+		return profileimg;
+	}
+	public void setProfileimg(String profileimg) {
+		this.profileimg = profileimg;
+	}
+}
